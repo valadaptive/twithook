e4eed2b2fc9cf358374aa412268f19f7d01229f8
valadaptive <valadaptive@protonmail.com>
2022-08-16 19:38:27 -0400

    Refactor to TypeScript

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
 # syntax=docker/dockerfile:1
 
-FROM node:18-alpine
-COPY ./package.json ./
-COPY ./package-lock.json ./
+FROM node:18-alpine AS build
+COPY ./package.json ./package-lock.json ./tsconfig.json ./
 RUN npm ci
-COPY . .
+COPY ./src ./src
+RUN npm run build
+
+FROM node:18-alpine
+COPY --from=build ./build ./package.json ./
 
 CMD [ "npm", "start" ]
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,45 @@
 import dotenv from 'dotenv';
-import {TwitterApi, ApiResponseError} from 'twitter-api-v2';
+import {TwitterApi, ApiResponseError, UserV2} from 'twitter-api-v2';
 import {WebhookClient} from 'discord.js';
 import sqlite3 from 'better-sqlite3';
 
 dotenv.config();
 
-const twitter = new TwitterApi(process.env.TWITTER_BEARER_TOKEN);
-const client = new WebhookClient({url: process.env.WEBHOOK_URL});
-const db = sqlite3(process.env.DB_FILE ?? 'data.db');
+function orError<T> (value: T | undefined, message: string): T {
+    if (typeof value === 'undefined') throw new Error(message);
+    return value;
+}
+
+function orDefault<T> (
+    value: T | undefined,
+    defaultValue: T): T {
+    return (typeof value === 'undefined') ? defaultValue : value;
+}
+
+function expectNumber (value: string | undefined): number | undefined {
+    const parsedValue = Number(value);
+    if (Number.isFinite(parsedValue))  return parsedValue;
+    return undefined;
+}
+
+const config = {
+    bearerToken: orError(process.env.TWITTER_BEARER_TOKEN, 'Expected bearer token'),
+    webhookUrl: orError(process.env.WEBHOOK_URL, 'Expected webhook URL'),
+    dbFile: orDefault(process.env.DB_FILE, 'data.db'),
+    maxTweetsAtOnce: orDefault(expectNumber(process.env.MAX_TWEETS_AT_ONCE), 10),
+    pollingRate: orError(expectNumber(process.env.POLLING_RATE), 'Expected polling rate'),
+    accounts: orError(((v: string | undefined): string[] | undefined => {
+        if (!v) return undefined;
+        const arr = v.split(/\s*,\s*/);
+        if (!arr.length) return undefined;
+        return arr;
+    })(process.env.ACCOUNTS), 'Expected accounts list')
+};
+
+const twitter = new TwitterApi(config.bearerToken);
+const client = new WebhookClient({url: config.webhookUrl});
+const db = sqlite3(config.dbFile);
 const webhookId = client.id;
-const maxTweetsAtOnce = Number.isFinite(Number(process.env.MAX_TWEETS_AT_ONCE)) ?
-    Number(process.env.MAX_TWEETS_AT_ONCE) :
-    10;
 
 const RATE_LIMIT = 1500;
 const RATE_LIMIT_WINDOW = 15 * 60;
@@ -29,9 +57,16 @@ db.exec(`
     )
 `);
 
+type UpdateLatest = {
+    webhook_id: string,
+    twitter_id: string,
+    latest_tweet_id: string
+};
+
 const statements = {
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
     updateLatest: (() => {
-        const updateLatestStmt = db.prepare(`INSERT INTO latest VALUES (
+        const updateLatestStmt = db.prepare<UpdateLatest>(`INSERT INTO latest VALUES (
             @webhook_id,
             @twitter_id,
             @latest_tweet_id
@@ -39,49 +74,38 @@ const statements = {
             webhook_id=excluded.webhook_id,
             twitter_id=excluded.twitter_id,
             latest_tweet_id=@latest_tweet_id`);
-        return db.transaction(items => {
-            for (const {webhookId, twitterId, latestId} of items) {
-                updateLatestStmt.run({webhook_id: webhookId, twitter_id: twitterId, latest_tweet_id: latestId});
-            };
+        return db.transaction((items: UpdateLatest[]) => {
+            for (const item of items) {
+                updateLatestStmt.run(item);
+            }
         });
     })(),
     getLatest: db.prepare(`SELECT latest_tweet_id FROM latest WHERE webhook_id = @webhookId AND twitter_id = @twitterId`)
 };
 
-const pollingRate = Number(process.env.POLLING_RATE);
-if (!Number.isFinite(pollingRate)) {
-    throw new Error('Polling rate unspecified');
-}
-
-const ACCOUNTS = process.env.ACCOUNTS?.split(/\s*,\s*/);
-if (!ACCOUNTS || !ACCOUNTS.length) {
-    throw new Error('No accounts specified');
-}
-
 // Number of times the poll function will be called per ratelimit window
-const execsPerWindow = RATE_LIMIT_WINDOW / pollingRate;
+const execsPerWindow = RATE_LIMIT_WINDOW / config.pollingRate;
 // Number of API endpoint requests per ratelimit window
-const fetchesPerWindow = execsPerWindow * ACCOUNTS.length;
+const fetchesPerWindow = execsPerWindow * config.accounts.length;
 
-const execsPerMonthlyWindow = MONTHLY_RATE_LIMIT_WINDOW / pollingRate;
-const fetchesPerMonthlyWindow = execsPerMonthlyWindow * ACCOUNTS.length * 10;
+const execsPerMonthlyWindow = MONTHLY_RATE_LIMIT_WINDOW / config.pollingRate;
+const fetchesPerMonthlyWindow = execsPerMonthlyWindow * config.accounts.length * 10;
 
-console.log(`Watching accounts ${ACCOUNTS.join(', ')}`);
+console.log(`Watching accounts ${config.accounts.join(', ')}`);
 console.log(`${fetchesPerWindow} API requests per ratelimit window (maximum is ${RATE_LIMIT})`);
 console.log(`${fetchesPerMonthlyWindow} API requests per monthly ratelimit window (maximum is ${MONTHLY_RATE_LIMIT})`);
 if (fetchesPerWindow > RATE_LIMIT || fetchesPerMonthlyWindow > MONTHLY_RATE_LIMIT) {
     throw new Error('API request rate exceeds rate limit');
 }
 
-const fetchedUsers = (await twitter.v2.usersByUsernames(ACCOUNTS, {'user.fields': ['profile_image_url', 'description', 'protected']})).data;
-
-const fetchTweets = async () => {
+const fetchTweets = async (users: UserV2[]): Promise<void> => {
     try {
-        const latestUpdates = [];
+        const latestUpdates: UpdateLatest[] = [];
         const newTweets = [];
-        for (const user of fetchedUsers) {
+        for (const user of users) {
             const {id} = user;
-            const latestResult = statements.getLatest.get({webhookId, twitterId: id});
+            const latestResult = statements.getLatest.get({
+                webhookId, twitterId: id}) as {latest_tweet_id: string} | undefined;
             const latest = latestResult?.latest_tweet_id;
             const {tweets} = await twitter.v2.userTimeline(id, {
                 exclude: ['retweets', 'replies'],
@@ -90,7 +114,7 @@ const fetchTweets = async () => {
             });
             // No tweets :(
             if (!tweets.length) continue;
-            latestUpdates.push({webhookId, twitterId: id, latestId: tweets[0].id});
+            latestUpdates.push({webhook_id: webhookId, twitter_id: id, latest_tweet_id: tweets[0].id});
 
             // Our first time running
             if (!latest) continue;
@@ -103,9 +127,9 @@ const fetchTweets = async () => {
 
             if (i > 0) console.log(`${i} new tweets from @${user.username}`);
 
-            if (tweets.length >= maxTweetsAtOnce) {
+            if (tweets.length >= config.maxTweetsAtOnce) {
                 await client.send({
-                    content: `Too many tweets! Displaying ${maxTweetsAtOnce} most recent.`,
+                    content: `Too many tweets! Displaying ${config.maxTweetsAtOnce} most recent.`,
                     username: user.name,
                     avatarURL: user.profile_image_url
                 });
@@ -133,5 +157,11 @@ const fetchTweets = async () => {
     }
 };
 
-setInterval(fetchTweets, pollingRate * 1000);
-fetchTweets();
+const main = async (): Promise<void> => {
+    const users = (await twitter.v2.usersByUsernames(
+        config.accounts, {'user.fields': ['profile_image_url', 'description', 'protected']})).data;
+    setInterval(() => void fetchTweets(users), config.pollingRate * 1000);
+    void fetchTweets(users);
+};
+
+void main();
